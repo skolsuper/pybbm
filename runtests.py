b72a6392e7b35cf4f113cbbb6f40b58a7b5f4a14
James Keys <skolsuper@gmail.com>
2016-01-01 11:22:48 +0700

    Drop django 1.4 and related paraphernalia

--- runtests.py
+++ runtests.py
@@ -1,40 +1,28 @@
-#!/usr/bin/env python
-import django
-import sys
 import os
+import sys
 from optparse import OptionParser
+
+import django
 from django.conf import settings
-try:
-    import south
-    south_installed = True
-except ImportError:
-    south_installed = False
+from django.test.utils import get_runner
 
 project_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test/test_project')
 sys.path.insert(0, project_dir)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_project.settings')
 
-try:
-    from django.test.runner import DiscoverRunner as Runner
-except ImportError:
-    from django.test.simple import DjangoTestSuiteRunner as Runner
-
 
 def runtests(*test_args, **kwargs):
-    if django.VERSION[:2] >= (1, 7):
-        django.setup()
-    if 'south' in settings.INSTALLED_APPS and south_installed:
-        from south.management.commands import patch_for_test_db_setup
-        patch_for_test_db_setup()
-
     if not test_args:
         test_args = ['pybb']
 
+    django.setup()
+    Runner = get_runner(settings)
     test_runner = Runner(verbosity=kwargs.get('verbosity', 1), interactive=kwargs.get('interactive', False),
                          failfast=kwargs.get('failfast'))
     failures = test_runner.run_tests(test_args)
     sys.exit(failures)
+
 
 if __name__ == '__main__':
     parser = OptionParser()
