bba550d349a2b62294baaebfc9214c7a364945e1
James Keys <skolsuper@gmail.com>
2016-01-02 10:26:25 +0700

    Specify models imports

--- pybb/models/__init__.py
+++ pybb/models/__init__.py
@@ -1,8 +1,8 @@
-from .attachment import *
-from .category import *
-from .forum import *
-from .poll import *
-from .post import *
-from .profile import *
-from .topic import *
-from .trackers import *
+from .attachment import Attachment
+from .category import Category
+from .forum import Forum
+from .poll import PollAnswer, PollAnswerUser
+from .post import Post
+from .profile import Profile
+from .topic import Topic
+from .trackers import TopicReadTracker, ForumReadTracker
