2af12525c8cdf75de4aaedf806e2b87a7baf1214
James Keys <skolsuper@gmail.com>
2016-01-09 22:06:02 +0700

    Fix and clean up after attachment tests.

--- pybb/tests/test_attachments.py
+++ pybb/tests/test_attachments.py
@@ -8,24 +8,29 @@ from django.core.urlresolvers import reverse
 from django.test import override_settings
 from rest_framework.test import APITestCase
 
-from pybb.models import Post
+from pybb.models import Post, Category, Forum, Topic
+from pybb.tests.utils import User
 
 FILE_NAME = os.path.join(os.path.dirname(__file__), '../static', 'pybb', 'img', 'attachment.png')
 
 
 @override_settings(PYBB_ATTACHMENT_ENABLE=True, PYBB_PREMODERATION=False)
 class AttachmentTest(APITestCase):
-    def setUp(self):
-        self.create_user()
-        self.create_initial()
 
     def test_attachments(self):
+        user = User.objects.create_user('zeus', 'zeus@localhost', 'zeus')
+        category = Category.objects.create(name='foo')
+        forum = Forum.objects.create(name='xfoo', description='bar', category=category)
+        topic = Topic.objects.create(name='etopic', forum=forum, user=user)
         add_post_url = reverse('pybb:add_post')
-        self.client.force_authenticate(self.user)
+        self.client.force_authenticate(user)
         with open(FILE_NAME, 'rb') as fp:
-            values = {'topic': self.topic.id, 'body': 'test attachment', 'attachment': fp}
+            values = {'topic': topic.id, 'body': 'test attachment', 'attachment': fp}
             response = self.client.post(add_post_url, values, follow=True)
         self.assertEqual(response.status_code, 201)
         self.assertIn('attachment', response.data)
         post = Post.objects.get(body='test attachment')
         self.assertIsNotNone(post.attachment)
+
+        # cleanup
+        post.attachment.delete()
