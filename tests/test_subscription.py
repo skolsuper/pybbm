6d125adf263eafe92d201c3fdbe46dfd5441c1b1
James Keys <skolsuper@gmail.com>
2016-01-24 12:14:43 +0700

    Pass subscriptions tests.

--- tests/test_subscription.py
+++ tests/test_subscription.py
@@ -8,9 +8,9 @@ from pybb.models import Post
 
 
 def test_subscription(admin_user, django_user_model, api_client, topic):
-    user2 = django_user_model.objects.create_user(username='user2', password='user2', email='user2@someserver.com')
-    user3 = django_user_model.objects.create_user(username='user3', password='user3', email='user3@example.com')
-
+    user2 = django_user_model.objects.create_user(username='user2', password='user2', email='user2@hotmail.com')
+    user3 = django_user_model.objects.create_user(username='user3', password='user3', email='user3@gmail.com')
+    Post.objects.create(topic=topic, user=admin_user, user_ip='0.0.0.0', body='A first post is needed')
     api_client.force_authenticate(user2)
     subscribe_url = reverse('pybb:add_subscription', args=[topic.id])
     response = api_client.get(subscribe_url)
@@ -33,32 +33,36 @@ def test_subscription(admin_user, django_user_model, api_client, topic):
     assert response.status_code == 201
     new_post = Post.objects.get(body=values['body'])
 
-    # there should only be one email in the outbox (to user2) because @example.com are ignored
-    assert len(mail.outbox) == 1
+    assert len(mail.outbox) == 2
     assert mail.outbox[0].to[0] == user2.email
     assert all([new_post.get_absolute_url() in msg.body for msg in mail.outbox])
 
+
+def test_unsubscribe(api_client, user, topic):
+    topic.subscribers.add(user)
+    assert user in topic.subscribers.all()
+
     # unsubscribe
-    api_client.force_authenticate(user2)
+    api_client.force_authenticate(user)
     response = api_client.get(reverse('pybb:delete_subscription', args=[topic.id]))
     assert response.status_code == 405, 'Need to make a post request to unsubscribe'
     response = api_client.post(reverse('pybb:delete_subscription', args=[topic.id]))
     assert response.status_code == 200
-    assert user2 not in topic.subscribers.all()
+    assert user not in topic.subscribers.all()
 
 
 def test_subscription_disabled(settings, admin_user, django_user_model, api_client, topic):
     settings.PYBB_DISABLE_SUBSCRIPTIONS = True
+    Post.objects.create(topic=topic, user=admin_user, user_ip='0.0.0.0', body='A first post is needed')
+
     user2 = django_user_model.objects.create_user(username='user2', password='user2', email='user2@someserver.com')
     user3 = django_user_model.objects.create_user(username='user3', password='user3', email='user3@example.com')
+    topic.subscribers.add(user3)
 
     api_client.force_authenticate(user2)
     subscribe_url = reverse('pybb:add_subscription', args=[topic.id])
-
     response = api_client.post(subscribe_url, follow=True)
     assert response.status_code == 404
-
-    topic.subscribers.add(user3)
 
     # create a new reply (with another user)
     api_client.force_authenticate(admin_user)
